de8d10297a353ba23dc8a83e93f55af8d7e3d678
Arseny Latyshev <arseny.latyshev@gmail.com>
2023-09-09 16:49:31 +0300

    Update CellState.java

--- src/main/java/com/greentree/example/telegram/ai/CellState.java
+++ src/main/java/com/greentree/example/telegram/ai/CellState.java
@@ -1,5 +1,23 @@
 package com.greentree.example.telegram.ai;
 
 public enum CellState {
-    X, O, Empty
+    X {
+        @Override
+        public CellState toInverse() {
+            return O;
+        }
+    }, O {
+        @Override
+        public CellState toInverse() {
+            return X;
+        }
+    }, Empty {
+        @Override
+        public CellState toInverse() {
+            throw new UnsupportedOperationException("CellState.Empty.toInverse()");
+        }
+    };
+
+    public abstract CellState toInverse();
+
 }
