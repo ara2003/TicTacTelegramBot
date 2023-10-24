3c0d6df9793606491d0d1e330188ba730627177b
Arseny Latyshev <arseny.latyshev@gmail.com>
2023-10-24 12:30:20 +0300

    Update Application.java

--- src/main/java/com/greentree/example/telegram/Application.java
+++ src/main/java/com/greentree/example/telegram/Application.java
@@ -3,7 +3,9 @@ package com.greentree.example.telegram;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.PropertySource;
 
+@PropertySource("classpath:application-secret.properties")
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class Application {
 
